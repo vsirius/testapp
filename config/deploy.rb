362ceea872d10f3e70d20304e7e451c842016318
Your Name <your@email.com>
2017-05-04 11:02:01 +0300

    ADD

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'testapp'
-set :repo_url, 'git@github.com:vsirius/testapp_2.git'
+set :repo_url, 'git@github.com:vsirius/appname.git'
 set :deploy_to, '/data/testapp'
 set :log_level, :debug
 set :linked_files, %w{config/database.yml}
