45eb4177b50980738adddf7734aa3065b03e4da5
Your Name <your@email.com>
2017-05-18 15:51:22 +0300

    add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-role :app, %w{deploy@54.93.228.152}
-role :web, %w{deploy@54.93.228.152}
-role :db,  %w{deploy@54.93.228.152}
+role :app, %w{deploy@52.59.187.207}
+role :web, %w{deploy@52.59.187.207}
+role :db,  %w{deploy@52.59.187.207}
